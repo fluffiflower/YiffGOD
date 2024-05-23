7fe0fd65d62095d00912f5f0bd1ba372d354e991
fluffiflower <110972626+fluffiflower@users.noreply.github.com>
2024-05-23 13:38:17 +0100

    Update index.js
    Thank tricky!!!

--- index.js
+++ index.js
@@ -1,20 +1,27 @@
-const { E6 } = require("furry-wrapper-neww")
-const discordjs = require("discord.js")
+const { E6 } = require("furry-wrapper-neww") //npm i furry-wrapper-neww
+const discordjs = require("discord.js") //npm i discord.js
+
 const bot = new discordjs.Client({intents: Object.keys(discordjs.GatewayIntentBits).map((a) => {return discordjs.GatewayIntentBits[a]})})
+
 //MAKE SURE TO ALLOW ALL INTENTS!!!!!! (idk why)
+
 bot.on('ready', async (client) => {
-	console.log('\u001b[32m[+] ' + bot.user.username + " has logged in")
-	var first = await bot.users.fetch('user id')
-	var second = await bot.users.fetch('user id')
-	first.createDM()
-	second.createDM()
-	setInterval(async () => { 	//You can enter whatever tags you like
-		E6(["male/male -female -eating_feces -scat -gore -watersports -loli -cub -fart -vore -feral"]).then((r) => {
-			first.send(r["file"]["url"])
-			second.send(r["file"]["url"])
-		})
-		.catch(console.log)
-	}, 3000)
+    // DEBUG--
+    console.log('\u001b[32m[+] ' + bot.user.username + " has logged in")
+
+    var first = await bot.users.fetch('user id')
+    var second = await bot.users.fetch('user id')
+
+    first.createDM() //First user the bot will dm to.
+    second.createDM() //Second user the bot will dm to.
+
+    setInterval(async () => {     //You can enter whatever tags you like
+        E6(["male/male -female -eating_feces -scat -gore -watersports -loli -cub -fart -vore -feral"]).then((r) => {
+            first.send(r["file"]["url"])
+            second.send(r["file"]["url"])
+        })
+        .catch(console.log)
+    }, 3000)
 })
 
 bot.login("TOKEN HERE") //Put in brackets your discord bot token!!
